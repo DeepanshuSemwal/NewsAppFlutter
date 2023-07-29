89366a6b2b5fdd06d4a87ed8cb9728571d484b4c
Deepanshu <semwaldeepanshu@gmail.com>
2023-07-29 15:59:47 +0530

    2nd

--- lib/api_services/api_service.dart
+++ lib/api_services/api_service.dart
@@ -5,13 +5,11 @@ import '../modals/get_modal.dart';
 
 class ApiService {
 
-  final endPointUrl ="https://newsapi.org/v2/everything?q=bitcoin&apiKey=85940a4d7b23488ba7ecd9e9e7c6533e";
   Future<List<Article>> getArticle() async {
-    //Response res = await http.get(endPointUrl as Uri);
-    final uri=Uri.parse(endPointUrl);
-    final res=await http.get(uri);
-    if (res.statusCode == 200) {
-      Map<String, dynamic> json = jsonDecode(res.body);
+
+    final response = await http.get(Uri.parse("https://newsapi.org/v2/everything?q=bitcoin&apiKey=85940a4d7b23488ba7ecd9e9e7c6533e"));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
       List<dynamic> body = json['articles'];
       List<Article> articles = body.map((dynamic item) => Article.fromJson(item)).toList();
 
